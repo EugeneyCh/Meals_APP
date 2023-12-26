5be2ae3fcd0697b15747663987e021384d5e8695
EugeneyCh <eugeney@ua.fm>
2023-12-26 17:40:34 +0200

    Second_Day

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,9 +1,12 @@
 import { FlatList } from "react-native";
+import CategoryGridTile from "./components/CategoryGridTile";
 
 import { CATEGORIES } from "../data/dummy-data";
 
 function renderCategoryItem(item) {
-  return;
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 function CategoriesScreen() {
